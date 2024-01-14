6148caaa9cb086340c39328060193fe8bb89dd4e
Tatyane Gonçalves <tatygoncalves97@gmail.com>
2024-01-14 13:03:43 -0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Proj.16-FAQ-Accordion
- 
+# Projeto 16 | FAQ Accordion
+## Bem-vindo(a)!
+Obrigada por conferir meu resultado do desafio Clipboard landing page do Frontend Mentor.
+
+Essa é uma solução do para o [desafio do FAQ Accordion](https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz)
+
+URL do site ativo: [FAQ Accordion](https://tatyane-goncalves.github.io/16-FAQ-Accordion/)
+
+### Autor
+- Mentor de Frontend - [@Tatyane-Goncalves](https://www.frontendmentor.io/profile/Tatyane-Goncalves)
+- LinkedIn - [@tatyanegoncalves](https://www.linkedin.com/in/tatyanegoncalves/)
---
+++ app.js
@@ -0,0 +1,20 @@
+const accordionBtns = document.querySelectorAll(".accordion-btn")
+
+accordionBtns.forEach(btn => {
+    btn.addEventListener("click", function() {
+        this.classList.toggle('active')
+        const accordionDescription = this.nextElementSibling
+        const plusicon = this.querySelector(".plus-icon")
+        const minusicon = this.querySelector(".minus-icon")
+
+        if(accordionDescription.style.maxHeight){
+            accordionDescription.style.maxHeight = null 
+            plusicon.style.display = 'block'
+            minusicon.style.display = 'none'
+        } else{
+            accordionDescription.style.maxHeight = accordionDescription.scrollHeight + 'px'
+            plusicon.style.display = 'none'
+            minusicon.style.display = 'block'
+        }
+    })
+})
